eb4f22ea393ee102b7a05610f1a55ed365bd9bc3
BhukyaShivaNayak <shivanayak1863@gmail.com>
2023-10-20 19:38:46 +0530

    text color is changed in footer section

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -13,17 +13,17 @@ const Footer = () => {
               alt='logo'
               width={150}
               height={46}
-              className='m-0'
+              className='m-0 text-coral-red'
             />
           </a>
-          <p className='mt-6 text-base leading-7 font-montserrat text-coral-red-400 sm:max-w-sm'>
+          <p className='mt-6 text-coral-red text-base leading-7 font-montserrat text-coral-red-400 sm:max-w-sm'>
             Get shoes ready for the new term at your nearest Nike store. Find
             Your perfect Size In Store. Get Rewards
           </p>
-          <div className='flex items-center gap-5 mt-8'>
+          <div className='flex  text-coral-red items-center gap-5 mt-8'>
             {socialMedia.map((icon) => (
               <div
-                className='flex justify-center items-center w-12 h-12 bg-coral-red rounded-full'
+                className='flex text-coral-red justify-center items-center w-12 h-12 bg-coral-red rounded-full'
                 key={icon.alt}
               >
                 <img src={icon.src} alt={icon.alt} width={24} height={24} />
@@ -44,7 +44,7 @@ const Footer = () => {
                     className='mt-3 font-montserrat text-base leading-normal text-coral-red-400 hover:text-slate-gray'
                     key={link.name}
                   >
-                    <a href={link.link}>{link.name}</a>
+                    <a className="text-coral-red" href={link.link}>{link.name}</a>
                   </li>
                 ))}
               </ul>
@@ -56,15 +56,15 @@ const Footer = () => {
       <div className='flex justify-between text-coral-red-400 mt-24 max-sm:flex-col max-sm:items-center'>
         <div className='flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer'>
           <img
-            src={copyrightSign}
+            src={"https://res.cloudinary.com/ccbp-nxtwave/image/upload/v1697810337/WhatsApp_Image_2023-10-20_at_7.26.53_PM_chnqw0.jpg"}
             alt='copyright sign'
             width={20}
             height={20}
             className='rounded-full m-0'
           />
-          <p>Copyright. All rights reserved.</p>
+          <p className="text-coral-red">Copyright. All rights reserved.</p>
         </div>
-        <p className='font-montserrat cursor-pointer'>Terms & Conditions</p>
+        <p className='font-montserrat cursor-pointer text-coral-red'>Terms & Conditions</p>
       </div>
     </footer>
   );
