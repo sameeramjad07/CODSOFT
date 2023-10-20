2280140b252c67c64e9bfb778057250d372fdc45
BhukyaShivaNayak <shivanayak1863@gmail.com>
2023-10-20 09:28:06 +0530

    changed the color of the text in the about section

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -16,14 +16,14 @@ const Footer = () => {
               className='m-0'
             />
           </a>
-          <p className='mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm'>
+          <p className='mt-6 text-base leading-7 font-montserrat text-coral-red-400 sm:max-w-sm'>
             Get shoes ready for the new term at your nearest Nike store. Find
             Your perfect Size In Store. Get Rewards
           </p>
           <div className='flex items-center gap-5 mt-8'>
             {socialMedia.map((icon) => (
               <div
-                className='flex justify-center items-center w-12 h-12 bg-white rounded-full'
+                className='flex justify-center items-center w-12 h-12 bg-coral-red rounded-full'
                 key={icon.alt}
               >
                 <img src={icon.src} alt={icon.alt} width={24} height={24} />
@@ -35,13 +35,13 @@ const Footer = () => {
         <div className='flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap'>
           {footerLinks.map((section) => (
             <div key={section.title}>
-              <h4 className='font-montserrat text-2xl leading-normal font-medium mb-6 text-white'>
+              <h4 className='font-montserrat text-2xl leading-normal font-medium mb-6 text-coral-red'>
                 {section.title}
               </h4>
               <ul>
                 {section.links.map((link) => (
                   <li
-                    className='mt-3 font-montserrat text-base leading-normal text-white-400 hover:text-slate-gray'
+                    className='mt-3 font-montserrat text-base leading-normal text-coral-red-400 hover:text-slate-gray'
                     key={link.name}
                   >
                     <a href={link.link}>{link.name}</a>
@@ -53,7 +53,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className='flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center'>
+      <div className='flex justify-between text-coral-red-400 mt-24 max-sm:flex-col max-sm:items-center'>
         <div className='flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer'>
           <img
             src={copyrightSign}
